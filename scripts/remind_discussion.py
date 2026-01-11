d93a8459d0c46a561cb418e8ba3cad6d2ace8119
Graham Ardent <ghill@ardent-security.com>
2026-01-11 16:36:46 -0500

    Remove emoji from discussion reminder for Windows compatibility

---
+++ scripts/remind_discussion.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Remind developer to update discussion.md if design decisions were made."""
+import sys
+
+print("TIP: If this session involved design decisions, update docs/discussion.md")
+sys.exit(0)
